dbc88381db503289c4f073077bd194e63bb288bc
Marcelo <schenamarcelo@gmail.com>
2023-03-23 17:20:54 -0300

    chapter 2 and 3: first endpoint and running tests

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::net::TcpListener;
+use actix_web:: {web, App, HttpServer, HttpResponse};
+use actix_web::dev::Server;
+
+async fn health_check(/* _req: HttpRequest */) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+email: String,
+name: String
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server =  HttpServer::new( || {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
